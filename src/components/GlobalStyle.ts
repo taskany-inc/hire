c2d9b09a97c0c2f3ccc6dcaa8df1df5916181641
Viktor Prokopyev <vserprokopyev@sberbank.ru>
2023-06-08 13:09:27 +0300

    feat: starting nextjs project

---
+++ src/components/GlobalStyle.ts
@@ -0,0 +1,21 @@
+/* stylelint-disable */
+import { createGlobalStyle } from 'styled-components';
+import { backgroundColor, fontDisplay, textColor } from '@taskany/colors';
+
+export const GlobalStyle = createGlobalStyle`
+    html, body {
+        box-sizing: border-box;
+
+        font-family: ${fontDisplay};
+        color: ${textColor};
+
+        margin: 0;
+        padding: 0;
+
+        width: 100%;
+        height: 100%;
+
+        background-color: ${backgroundColor};
+        --background-color-context: ${backgroundColor};
+    }
+`;

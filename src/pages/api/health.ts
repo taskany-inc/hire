2a1ffb84d1d4fa87e5b6fd8551df0febb08622da
Viktor <61706821+nyet-ty@users.noreply.github.com>
2023-07-20 13:58:40 +0300

    fix: api helth always returns 200 (#80)

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -1,10 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { prisma } from '../../backend';
 import { getApiHandler } from '../../backend/api-handler.util';
 
 const handler = getApiHandler().get(async (_req: NextApiRequest, res: NextApiResponse) => {
-    await prisma.problem.findFirst();
     res.status(200).json('Ok');
 });
 

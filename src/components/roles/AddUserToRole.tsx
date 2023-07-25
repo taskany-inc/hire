f279a7805f49e10024e2a853091f8b72d6052e02
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2023-07-25 11:03:51 +0300

    fix: user list previews do not lay one on another

--- src/components/roles/AddUserToRole.tsx
+++ src/components/roles/AddUserToRole.tsx
@@ -1,6 +1,6 @@
-import { useState, VFC } from 'react';
+import { useCallback, useRef, useState, VFC } from 'react';
 import { User } from '@prisma/client';
-import { ModalPreview, AddIcon } from '@taskany/bricks';
+import { ModalPreview, AddIcon, useClickOutside, useKeyboard, KeyCode } from '@taskany/bricks';
 
 import { UserList } from '../users/UserList';
 import { useUserList } from '../../hooks/user-hooks';
@@ -15,15 +15,36 @@ type AddUserToRoleProps = {
 export const AddUserToRole: VFC<AddUserToRoleProps> = ({ title, onSelect, filterByIds }) => {
     const [open, setOpen] = useState(false);
 
-    const usersQuery = useUserList();
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const onClickOutside = useCallback(() => {
+        if (open) {
+            setOpen(false);
+        }
+    }, [setOpen, open]);
 
+    const usersQuery = useUserList();
+    useClickOutside(wrapperRef, onClickOutside);
+
+    const [onESC] = useKeyboard(
+        [KeyCode.Escape],
+        (event) => {
+            if (event.target && wrapperRef.current?.contains(event.target as Node) && open) {
+                setOpen(false);
+            }
+        },
+        {
+            capture: true,
+            event: 'keydown',
+        },
+    );
     return (
-        <>
+        <div ref={wrapperRef}>
             <AddIcon size="s" onClick={() => setOpen(true)} />
             <ModalPreview visible={open} onClose={() => setOpen(false)}>
                 <QueryResolver queries={[usersQuery]}>
                     {([users]) => (
                         <UserList
+                            {...onESC}
                             users={users.filter((user) => !filterByIds.includes(user.id))}
                             title={title}
                             action={{ icon: <AddIcon size="s" />, handler: onSelect }}
@@ -33,6 +54,6 @@ export const AddUserToRole: VFC<AddUserToRoleProps> = ({ title, onSelect, filter
                     )}
                 </QueryResolver>
             </ModalPreview>
-        </>
+        </div>
     );
 };

f5a2a8e6edb5dc5f51636adcf88919b23e742ef5
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2024-03-25 15:12:27 +0300

    feat(Section): link to cv on section

--- src/components/CandidateNameSubtitle/CandidateNameSubtitle.tsx
+++ src/components/CandidateNameSubtitle/CandidateNameSubtitle.tsx
@@ -14,14 +14,7 @@ interface Props {
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function CandidateNameSubtitle({ id, name }: Props) {
     return (
-        <Text
-            size="xl"
-            style={{
-                fontWeight: 'bold',
-                fontSize: '22px',
-                lineHeight: '28px',
-            }}
-        >
+        <Text weight="bold" size="l">
             {tr('Candidate')}:{' '}
             {nullable(
                 id,

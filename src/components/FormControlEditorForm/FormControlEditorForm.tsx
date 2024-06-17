fc8ca389ab05c65d396e80d5394ee9a4c28f1ec1
Anton Sabotovich <sab94a@yandex.ru>
2024-06-17 17:58:02 +0300

    fix: keep monaco files in repo

--- src/components/FormControlEditorForm/FormControlEditorForm.tsx
+++ src/components/FormControlEditorForm/FormControlEditorForm.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { editorLoader } from '@taskany/bricks';
 import { FormControlEditor as FormEditor } from '@taskany/bricks/harmony';
 
 import { tr } from './FormControlEditorForm.i18n';
+
+editorLoader.config({
+    paths: {
+        vs: '/monaco',
+    },
+});
 
 export const FormControlEditor = React.forwardRef<HTMLDivElement, React.ComponentProps<typeof FormEditor>>(
     ({ ...props }, ref) => (

293d63351eed0efe3a3d5d50c178f81a00a5f720
Goriany Igor <mr.q.u.b.e31@gmail.com>
2024-06-25 11:44:49 +0300

    fix: fix ts error

--- src/hooks/useQueryParamList.ts
+++ src/hooks/useQueryParamList.ts
@@ -1,11 +1,13 @@
 import { NextRouter, useRouter } from 'next/router';
 
-type UseQueryParamListReturn = [<Param extends string>(filter: Param) => string[], NextRouter];
+type GetQueryList = <Param extends string>(filter: Param) => string[];
+
+type UseQueryParamListReturn = [GetQueryList, NextRouter];
 
 export const useQueryParamList = (): UseQueryParamListReturn => {
     const router = useRouter();
     const getQueryList = <Param extends string>(filter: Param) =>
         router.query[filter] === undefined ? [] : [router.query[filter]].flat(1);
 
-    return [getQueryList, router];
+    return [getQueryList as GetQueryList, router];
 };

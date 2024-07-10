6722d4829f623bea48f605807b40090913d6d052
Goriany Igor <mr.q.u.b.e31@gmail.com>
2024-07-10 05:29:33 +0300

    refactor: use the user id instead of the user settings id

--- src/components/LayoutMain.tsx
+++ src/components/LayoutMain.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import styled from 'styled-components';
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/router';
-// import dynamic from 'next/dynamic';
 
 import { trpc } from '../trpc/trpcClient';
 

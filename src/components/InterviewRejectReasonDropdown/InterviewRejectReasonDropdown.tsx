99546ae38c4975e37e1a3437e78384153f17d444
Katrinputrina <putrina.katya@gmail.ru>
2024-08-09 17:21:53 +0300

    feat(InterviewRejectReasons): rewrite reject reasons to select and add a new reason

--- src/components/InterviewRejectReasonDropdown/InterviewRejectReasonDropdown.tsx
+++ src/components/InterviewRejectReasonDropdown/InterviewRejectReasonDropdown.tsx
@@ -1,17 +1,6 @@
-import { useCallback, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { RejectReason } from '@prisma/client';
-import { IconDownSmallSolid, IconUpSmallSolid } from '@taskany/icons';
-import {
-    Dropdown,
-    DropdownPanel,
-    DropdownTrigger,
-    MenuItem,
-    Text,
-    Button,
-    ListView,
-    ListViewItem,
-} from '@taskany/bricks/harmony';
-import { gray8 } from '@taskany/colors';
+import { Text, Select, SelectTrigger, SelectPanel, Input } from '@taskany/bricks/harmony';
 
 import s from './InterviewRejectReasonDropdown.module.css';
 import { tr } from './InterviewRejectReasonDropdown.i18n';
@@ -20,71 +9,67 @@ interface RejectInterviewStatusProps {
     rejectReasons: RejectReason[];
     onChangeRejectReasons?: (selected: string) => void;
 }
+
 export const InterviewRejectReasonDropdown = ({
     rejectReasons,
     onChangeRejectReasons,
 }: RejectInterviewStatusProps): JSX.Element => {
     const [isOpen, setIsOpen] = useState(false);
-    const standartOptions = rejectReasons?.map((option, index) => ({
-        text: option.text,
-        id: index,
-    }));
-    const [selected, setSelected] = useState(standartOptions[0].text);
+    const [search, setSearch] = useState<string>('');
+    const [selectedRejectReason, setSelectedRejectReason] = useState<string>();
 
-    const onStateClick = useCallback(
-        (option: RejectReason) => {
-            selected === option.text ? setSelected(standartOptions[0].text) : setSelected(option.text);
+    const items = useMemo(() => {
+        const result = rejectReasons.filter(({ text }) =>
+            text.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+        );
+        if (!result.length) {
+            result.push({
+                text: search,
+                id: -1,
+            });
+        }
 
-            onChangeRejectReasons?.(option.text);
-            setIsOpen(false);
-        },
-        [onChangeRejectReasons, selected, standartOptions],
-    );
+        return result.map((option, index) => ({ text: option.text, id: index.toString() }));
+    }, [rejectReasons, search]);
+
     return (
         <div className={s.DropdownRejectStatus}>
-            <Dropdown isOpen={isOpen} onClose={() => setIsOpen(false)}>
-                <DropdownTrigger
+            <Select
+                items={items}
+                isOpen={isOpen}
+                onChange={(item) => {
+                    const { text } = item[0];
+                    setSearch(text);
+                    setSelectedRejectReason(text);
+                    onChangeRejectReasons?.(text);
+                }}
+                onClose={() => setIsOpen(false)}
+                mode="single"
+                renderItem={({ item }) => <Text size="xs">{item.text}</Text>}
+            >
+                <SelectTrigger
+                    className={s.Input}
                     renderTrigger={(props) => (
-                        <div ref={props.ref} className={s.RejectInterviewHeaderStats}>
-                            <Text weight="bold" color={gray8} className={s.StandartComment}>
-                                {tr('Standard Comment')}
-                            </Text>
-                            <Button
-                                className={s.ButtonReject}
-                                view="default"
-                                text={selected}
-                                color={gray8}
-                                type="button"
+                        <div className={s.RejectInterviewHeaderStats}>
+                            <Input
+                                placeholder={tr('Choose reject reason or enter a new reason')}
+                                className={s.Iput}
+                                value={selectedRejectReason ?? search}
+                                onChange={(e) => {
+                                    setSelectedRejectReason(undefined);
+                                    setSearch(e.target.value);
+                                }}
                                 size="m"
-                                onClick={() => setIsOpen((val) => !val)}
-                                iconRight={
-                                    props.isOpen ? <IconUpSmallSolid size="s" /> : <IconDownSmallSolid size="s" />
-                                }
+                                onClick={props.onClick}
+                                ref={props.ref}
+                                view="default"
+                                title="text"
                             />
                         </div>
                     )}
                 />
-                <DropdownPanel className={s.DropdownPanelRejectStatus}>
-                    <ListView>
-                        {standartOptions?.map((option) => (
-                            <ListViewItem
-                                key={option.text}
-                                value={option}
-                                renderItem={({ active, hovered, ...props }) => (
-                                    <MenuItem
-                                        hovered={active || hovered}
-                                        onClick={() => onStateClick(option)}
-                                        key={option.id}
-                                        {...props}
-                                    >
-                                        {option.text}
-                                    </MenuItem>
-                                )}
-                            />
-                        ))}
-                    </ListView>
-                </DropdownPanel>
-            </Dropdown>
+                <SelectPanel placement="top-start" className={s.DropdownPanelRejectStatus} />
+            </Select>
         </div>
     );
 };

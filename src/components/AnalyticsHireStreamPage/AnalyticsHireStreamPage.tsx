554743170900894051fe7a706823f2d6c440b414
tsumo <tsumo@users.noreply.github.com>
2024-09-19 10:38:59 +0300

    feat(analytics filter): hide stream filter for stream page

--- src/components/AnalyticsHireStreamPage/AnalyticsHireStreamPage.tsx
+++ src/components/AnalyticsHireStreamPage/AnalyticsHireStreamPage.tsx
@@ -3,8 +3,6 @@ import { LayoutMain } from '../LayoutMain/LayoutMain';
 import { HiringBySectionType } from '../HiringBySectionType/HiringBySectionType';
 import { GradesByInterviewer } from '../GradesByInterviewer/GradesByInterviewer';
 import { AnalyticsFilterBar } from '../AnalyticsFilterBar/AnalyticsFilterBar';
-import { QueryResolver } from '../QueryResolver/QueryResolver';
-import { useHireStreams } from '../../modules/hireStreamsHooks';
 
 import { tr } from './AnalyticsHireStreamPage.i18n';
 import s from './AnalyticsHireStreamPage.module.css';
@@ -14,22 +12,16 @@ export interface HireStreamPageProps {
 }
 
 export const AnalyticsHireStreamPage = ({ stringIds }: HireStreamPageProps) => {
-    const hireStreamsQuery = useHireStreams();
-
     return (
-        <QueryResolver queries={[hireStreamsQuery]}>
-            {([hireStreams]) => (
-                <LayoutMain
-                    pageTitle={`${tr('Hiring by section type')} ${stringIds.hireStream}`}
-                    filterBar={<AnalyticsFilterBar hireStreams={hireStreams} title={tr('Hiring by section type')} />}
-                    backlink={Paths.ANALYTICS}
-                >
-                    <div className={s.AnalyticsHireStreamPage}>
-                        <HiringBySectionType hireStreamName={stringIds.hireStream} />
-                        <GradesByInterviewer hireStreamName={stringIds.hireStream} />
-                    </div>
-                </LayoutMain>
-            )}
-        </QueryResolver>
+        <LayoutMain
+            pageTitle={`${tr('Hiring by section type')} ${stringIds.hireStream}`}
+            filterBar={<AnalyticsFilterBar title={tr('Hiring by section type')} />}
+            backlink={Paths.ANALYTICS}
+        >
+            <div className={s.AnalyticsHireStreamPage}>
+                <HiringBySectionType hireStreamName={stringIds.hireStream} />
+                <GradesByInterviewer hireStreamName={stringIds.hireStream} />
+            </div>
+        </LayoutMain>
     );
 };

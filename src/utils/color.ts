a310118081c98f476e2496470d5d76c347211c46
Goriany Igor <mr.q.u.b.e31@gmail.com>
2024-09-23 18:17:15 +0300

    fix: tag color generator doesn't respect light theme

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -4,9 +4,10 @@ export const generateColor = () => {
     const ccc = new ColorContrastChecker();
 
     // default background
-    const backgroundColor = '#1b1b1c';
+    const backgroundColorDark = '#25252c';
+    const backgroundColorLight = '#eeeef1';
     // contrast ratio
-    const ratio = 6;
+    const ratio = 3;
 
     const genColor = () => `#${`${Math.random().toString(16)}000000`.substring(2, 8)}`;
     let generatedColor = '';
@@ -16,11 +17,15 @@ export const generateColor = () => {
         generatedColor = genColor();
         try {
             // our kawaii generator does not always produce a valid hex
-            valid = ccc.isLevelCustom(generatedColor, backgroundColor, ratio);
+            valid =
+                ccc.isLevelCustom(generatedColor, backgroundColorDark, ratio) &&
+                ccc.isLevelCustom(generatedColor, backgroundColorLight, ratio);
         } catch (e) {
             // and under the color-contrast-checker hood there is a validator that crashes with an error on bad hex.
             // Therefore, we generate until we're blue in the face.
-            valid = ccc.isLevelCustom(generatedColor, backgroundColor, ratio);
+            valid =
+                ccc.isLevelCustom(generatedColor, backgroundColorDark, ratio) &&
+                ccc.isLevelCustom(generatedColor, backgroundColorLight, ratio);
         }
     }
 

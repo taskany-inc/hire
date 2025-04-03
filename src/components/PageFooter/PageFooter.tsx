4e4297813d0e55bf04a254ea14fd5188dc33fc30
Katrinputrina <putrina.katya@gmail.ru>
2025-04-03 19:11:48 +0300

    fix(INTERNAL-1643): fix footer

--- src/components/PageFooter/PageFooter.tsx
+++ src/components/PageFooter/PageFooter.tsx
@@ -7,7 +7,7 @@ import { Link } from '../Link';
 import { defaultLocale, languages } from '../../utils/getLang';
 import { useEditUserSettings } from '../../modules/userHooks';
 import { trpc } from '../../trpc/trpcClient';
-import SheepLogoWithMessage from '../SheepLogoWithTips/SheepLogoWithTips';
+import SheepLogoWithTips from '../SheepLogoWithTips/SheepLogoWithTips';
 
 import { tr } from './PageFooter.i18n';
 
@@ -35,7 +35,7 @@ export const PageFooter: FC = () => {
 
     return (
         <Footer>
-            <FooterCopyright />
+            <FooterCopyright orgName="SD Hire" />
             <FooterMenu>
                 <Modal visible={openFeedbackForm} onClose={() => setOpenFeedbackForm(false)}>
                     <FeedbackCreateForm onClose={() => setOpenFeedbackForm(false)} />
@@ -57,7 +57,7 @@ export const PageFooter: FC = () => {
                 </Link>
             </FooterMenu>
 
-            <SheepLogoWithMessage />
+            <SheepLogoWithTips />
         </Footer>
     );
 };
